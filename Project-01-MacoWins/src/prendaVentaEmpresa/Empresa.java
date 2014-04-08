6159694360be8a18435ee8ed8f1e44c45bb28401
Martin Montenegro <martin.92m@gmail.com>
2014-04-07 21:51:09 -0300

    Terminada clase Empresa

--- Project-01-MacoWins/src/prendaVentaEmpresa/Empresa.java
+++ Project-01-MacoWins/src/prendaVentaEmpresa/Empresa.java
@@ -1,15 +1,35 @@
 package prendaVentaEmpresa;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+
 public class Empresa {
 	
 	private Double valorFijo;
+	private List<Venta> ventas;
 	
-	public Empresa (Double valor){
+	public Empresa (Double valor)
+	{
 		valorFijo = valor;
+		ventas = new ArrayList<Venta>();
 	}
 	
-	public Double getValorFijo(){
+	public Double getValorFijo()
+	{
 		return valorFijo;
+	}
+	
+	public void vender(Prenda unaPrenda, Integer unaCant)
+	{
+		ventas.add(new Venta(new Date(), unaPrenda, unaCant));
+	}
+	
+	public Double gananciaTotal()
+	{
+		// SUM en Java 8
+		return ventas.stream().mapToDouble(Venta::monto).sum();
 	}
 }
 
