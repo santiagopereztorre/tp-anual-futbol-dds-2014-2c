69c7d6c2d333d70b4aef5d21fcd817534fe192c7
Martin Montenegro <martin.92m@gmail.com>
2014-04-07 21:36:58 -0300

    En Venta, agregados atributos faltantes e implementacion de monto()

--- Project-01-MacoWins/src/prendaVentaEmpresa/Venta.java
+++ Project-01-MacoWins/src/prendaVentaEmpresa/Venta.java
@@ -1,5 +1,22 @@
 package prendaVentaEmpresa;
 
-public class Venta {
+import java.util.Date;
 
+public class Venta 
+{
+	private Date fecha;
+	private Prenda prenda;
+	private Integer cantidad;
+	
+	public Venta(Date fecha, Prenda prenda, Integer cantidad) 
+	{
+		this.fecha = fecha;
+		this.prenda = prenda;
+		this.cantidad = cantidad;
+	}
+	
+	public Double monto()
+	{
+		return prenda.precioFinal() * cantidad;
+	}
 }
