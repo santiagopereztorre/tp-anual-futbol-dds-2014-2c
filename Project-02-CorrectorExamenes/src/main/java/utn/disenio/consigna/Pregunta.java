5374cf52e4ecfa236ee41a45d71fff651d93be01
ramabit <f.e.ramirez94@gmail.com>
2014-04-16 20:35:48 -0300

    Codeando interfaz consigna

---
+++ Project-02-CorrectorExamenes/src/main/java/utn/disenio/consigna/Pregunta.java
@@ -0,0 +1,26 @@
+package utn.disenio.consigna;
+
+public class Pregunta implements Consigna {
+
+	@Override
+	public int peso() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public String respuestaCorrecta() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String respuestaAlumno() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+
+
+}
