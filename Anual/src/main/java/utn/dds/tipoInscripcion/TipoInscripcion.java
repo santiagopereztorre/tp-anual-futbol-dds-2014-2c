49f31e5b0407e6be770dfe4fb07efbdf5069b12b
ramabit <f.e.ramirez94@gmail.com>
2014-04-30 19:48:02 -0300

    Planteando un posible refactoring a debatir

--- Anual/src/main/java/utn/dds/tipoInscripcion/TipoInscripcion.java
+++ Anual/src/main/java/utn/dds/tipoInscripcion/TipoInscripcion.java
@@ -3,6 +3,14 @@ import utn.dds.partido.Partido;
 
 public interface TipoInscripcion {
 	
+	
+	/* Esto no me copa, ¿xq todos los tipos de inscripcion 
+	 * 					deberian entender el metodo cumpleCondicion?
+	 * Solo el condicional lo usa.
+	 * Voto por meter un if, aunque no nos guste usar ifs.
+	 *  Firma: Fer
+	 */
+	
 	public boolean cumpleCondicion(Partido partido);
 	
 }
