2df36d9402c62c105fda106927f9232c20454609
MaximilianoFelice <maximilianofelice@gmail.com>
2014-05-01 12:26:07 -0300

    Refactor: Eliminado comment con cuestionamiento y abierto un nuevo issue.

--- Anual/src/main/java/utn/dds/tipoInscripcion/TipoInscripcion.java
+++ Anual/src/main/java/utn/dds/tipoInscripcion/TipoInscripcion.java
@@ -3,14 +3,6 @@ import utn.dds.partido.Partido;
 
 public interface TipoInscripcion {
 	
-	
-	/* Esto no me copa, ¿xq todos los tipos de inscripcion 
-	 * 					deberian entender el metodo cumpleCondicion?
-	 * Solo el condicional lo usa.
-	 * Lo puse en otro branch
-	 *  Firma: Fer
-	 */
-	
 	public boolean cumpleCondicion(Partido partido);
 	
 }
