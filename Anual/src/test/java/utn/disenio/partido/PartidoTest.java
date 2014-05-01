8ebb8fec93e2fe95999d28206800653966cfb90a
ramabit <f.e.ramirez94@gmail.com>
2014-05-01 12:51:37 -0300

    Arreglando test de inscripcion jugadores
    3 tests hacian cosas similares, mejor unificarlo en 1. Ademas, se
    utilizaba el nombre listaInscriptosEstandar para los 3 tipos de
    inscripcion, lo cual estaba mal

--- Anual/src/test/java/utn/disenio/partido/PartidoTest.java
+++ Anual/src/test/java/utn/disenio/partido/PartidoTest.java
@@ -28,39 +28,29 @@ public class PartidoTest {
 	}
 		
 	@Test
-	public void inscribirEstandar() {
+	public void inscribirJugadores() {
 		Jugador juan = new Jugador();
 		Jugador carlitos = new Jugador();
+		Jugador pepe = new Jugador();
+		Jugador moncho = new Jugador();
+		Jugador lalo = new Jugador();
 		
+		lalo.inscribirseAPartidoConTipoInscripcion(solterosVsCasados, new Solidaria());
+		pepe.inscribirseAPartidoConTipoInscripcion(solterosVsCasados, new Condicional());
+		moncho.inscribirseAPartidoConTipoInscripcion(solterosVsCasados, new Condicional());
 		juan.inscribirseAPartidoConTipoInscripcion(solterosVsCasados, new Estandar());
 		carlitos.inscribirseAPartidoConTipoInscripcion(solterosVsCasados, new Estandar());
 		
 		List<Inscripcion> listaInscriptosEstandar = solterosVsCasados.getInscriptosDeTipo(Estandar.class);
+		List<Inscripcion> listaInscriptosSolidarios = solterosVsCasados.getInscriptosDeTipo(Solidaria.class);
+		List<Inscripcion> listaInscriptosCondicionales = solterosVsCasados.getInscriptosDeTipo(Condicional.class);
+		
 		Assert.assertEquals(listaInscriptosEstandar.size(), 2);
+		Assert.assertEquals(listaInscriptosSolidarios.size(), 1);
+		Assert.assertEquals(listaInscriptosCondicionales.size(), 2);
 	}
 	
-	@Test
-	public void inscribirSolidarios(){
-		Jugador lalo = new Jugador();
 		
-		lalo.inscribirseAPartidoConTipoInscripcion(solterosVsCasados, new Solidaria());
-		
-		List<Inscripcion> listaInscriptosEstandar = solterosVsCasados.getInscriptosDeTipo(Solidaria.class);
-		Assert.assertEquals(listaInscriptosEstandar.size(), 1);
-	}
-	
-	@Test
-	public void inscribirCondicionales(){
-		Jugador pepe = new Jugador();
-		Jugador moncho = new Jugador();
-		
-		pepe.inscribirseAPartidoConTipoInscripcion(solterosVsCasados, new Condicional());
-		moncho.inscribirseAPartidoConTipoInscripcion(solterosVsCasados, new Condicional());
-		
-		List<Inscripcion> listaInscriptosEstandar = solterosVsCasados.getInscriptosDeTipo(Condicional.class);
-		Assert.assertEquals(listaInscriptosEstandar.size(), 2);
-	}
-	
 	@Test (expected = NoHayVacantesException.class)
 	public void inscribirMasDeDiezEstandar(){
 		
