398c7181747effa901bca38de9b80fa6a292fe04
ramabit <f.e.ramirez94@gmail.com>
2014-05-14 17:51:24 -0300

    Agregada lista de infracciones a jugador

--- Anual/src/main/java/utn/dds/jugador/Jugador.java
+++ Anual/src/main/java/utn/dds/jugador/Jugador.java
@@ -1,13 +1,22 @@
 package utn.dds.jugador;
 
+import java.util.List;
+
+import utn.dds.infraccion.Infraccion;
 import utn.dds.partido.*;
 import utn.dds.tipoInscripcion.*;
 
 
 public class Jugador {
 	
+	private List<Infraccion> infracciones;
+	
 	public void inscribirseAPartidoConTipoInscripcion(Partido partido, TipoInscripcion tipoInscripcion){
 		partido.inscribirJugador(this, tipoInscripcion);
+	}
+
+	public int cantidadInfracciones() {
+		return infracciones.size();
 	}
 	
 	
