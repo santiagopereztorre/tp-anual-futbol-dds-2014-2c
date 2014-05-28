c9b4aea9d1164cac7b95fcf202314c398c7929d8
Martin Montenegro <martin.92m@gmail.com>
2014-05-28 19:01:34 -0300

    Refactor de NoJugaronJuntosException Modificado el raise en Jugador

--- Anual/src/main/java/utn/dds/jugador/excepciones/NoJugaronJuntosException.java
+++ Anual/src/main/java/utn/dds/jugador/excepciones/NoJugaronJuntosException.java
@@ -2,4 +2,15 @@ package utn.dds.jugador.excepciones;
 
 public class NoJugaronJuntosException extends RuntimeException {
 
+	public NoJugaronJuntosException()
+	{
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public NoJugaronJuntosException(String message)
+	{
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
 }
