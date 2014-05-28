64ca194ebb3563ba3c6531fd6aa05d082769019c
MaximilianoFelice <maximilianofelice@gmail.com>
2014-05-28 20:47:06 -0300

    Agregados setters y getters de rechazos.

--- Anual/src/main/java/utn/dds/admin/Rechazo.java
+++ Anual/src/main/java/utn/dds/admin/Rechazo.java
@@ -11,8 +11,35 @@ public class Rechazo
 	
 	Rechazo(Jugador unJugador, TipoInscripcion inscripcion, String motivo)
 	{
-		jugador = unJugador;
+		setJugador(unJugador);
+		this.setInscripcion(inscripcion);
+		this.setMotivo(motivo);
+	}
+
+	
+	/* Setters y Getters */
+	public Jugador getJugador() {
+		return jugador;
+	}
+
+	public void setJugador(Jugador jugador) {
+		this.jugador = jugador;
+	}
+
+	public TipoInscripcion getInscripcion() {
+		return inscripcion;
+	}
+
+	public void setInscripcion(TipoInscripcion inscripcion) {
 		this.inscripcion = inscripcion;
+	}
+
+	public String getMotivo() {
+		return motivo;
+	}
+
+	public void setMotivo(String motivo) {
 		this.motivo = motivo;
 	}
+	
 }
