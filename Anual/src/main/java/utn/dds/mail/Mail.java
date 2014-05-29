30b75eb9def926774e2c0062c6a154bc1d56a8e6
ramabit <f.e.ramirez94@gmail.com>
2014-05-28 21:22:09 -0300

    Fix expresividad mail

--- Anual/src/main/java/utn/dds/mail/Mail.java
+++ Anual/src/main/java/utn/dds/mail/Mail.java
@@ -2,14 +2,14 @@ package utn.dds.mail;
 
 public class Mail {
 	
-	String para;
-	String de;
+	String destinatario;
+	String origen;
 	String mensaje;
 
-	public Mail(String para, String de, String mensaje)
+	public Mail(String destino, String origen, String mensaje)
 	{
-		this.para = para;
-		this.de = de;
+		this.destinatario = destino;
+		this.origen = origen;
 		this.mensaje = mensaje;
 	}
 	
