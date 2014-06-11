d27e4e34703749510e97798afbc2d6648ee2b6e4
Santiago <saantii.28@hotmail.com>
2014-06-11 18:42:08 -0300

    Modificaciones a Dividor y ParImpar Le agrege comportamiento a ParImpar de forma imperativa Cambie la firma de Dividor

--- Anual/src/main/java/utn/dds/divisores/Divisor.java
+++ Anual/src/main/java/utn/dds/divisores/Divisor.java
@@ -1,15 +1,14 @@
 package utn.dds.divisores;
 
-import java.util.Hashtable;
 import java.util.List;
 
 import utn.dds.inscripcion.Inscripcion;
-import utn.dds.jugador.Jugador;
+import utn.dds.jugador.JugadorConCalificacion;
 
 public interface Divisor {
 
 	void armarEquipos(List<Inscripcion> equipo1, List<Inscripcion> equipo2,
-			Hashtable<Jugador, Integer> inscriptosCalificados);
+			List<JugadorConCalificacion> jugadoresCalificados);
 
 	
 }
--- Anual/src/main/java/utn/dds/divisores/ParImpar.java
+++ Anual/src/main/java/utn/dds/divisores/ParImpar.java
@@ -1,19 +1,34 @@
 package utn.dds.divisores;
 
-import java.util.Hashtable;
 import java.util.List;
 
 import utn.dds.inscripcion.Inscripcion;
-import utn.dds.jugador.Jugador;
+import utn.dds.jugador.JugadorConCalificacion;
 
 public class ParImpar implements Divisor {
 
 	@Override
 	public void armarEquipos(List<Inscripcion> equipo1,
 			List<Inscripcion> equipo2,
-			Hashtable<Jugador, Integer> inscriptosCalificados) {
-		// TODO Auto-generated method stub
+			List<JugadorConCalificacion> jugadoresCalificados) 
+	{
+		List<JugadorConCalificacion> jugadoresCalificadosOrdenados = (List<JugadorConCalificacion>) jugadoresCalificados.stream().sorted();
+		for (int i = 0; i < jugadoresCalificadosOrdenados.size(); i ++)
+		{
+			if (esPar(i))
+			{
+				equipo1.add(jugadoresCalificadosOrdenados.get(i).getInscripcion());
+			}
+			else
+			{
+				equipo2.add(jugadoresCalificadosOrdenados.get(i).getInscripcion());
+			}
+		}
 
+	}
+
+	private boolean esPar(int numero) {
+		if (numero%2==0) return true; else return false;
 	}
 
 }
