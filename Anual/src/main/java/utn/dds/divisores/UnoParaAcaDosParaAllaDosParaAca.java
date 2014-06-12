776bbc0498842f4d0baf5b25bfa3e130eccd14c6
Santiago <saantii.28@hotmail.com>
2014-06-11 21:02:09 -0300

    Agregada funcionalidad divisor2

--- Anual/src/main/java/utn/dds/divisores/UnoParaAcaDosParaAllaDosParaAca.java
+++ Anual/src/main/java/utn/dds/divisores/UnoParaAcaDosParaAllaDosParaAca.java
@@ -1,5 +1,6 @@
 package utn.dds.divisores;
 
+import java.util.Collections;
 import java.util.Hashtable;
 import java.util.List;
 
@@ -12,8 +13,17 @@ public class UnoParaAcaDosParaAllaDosParaAca implements Divisor {
 	@Override
 	public void armarEquipos(List<Jugador> equipo1, List<Jugador> equipo2,
 			List<JugadorConCalificacion> jugadoresCalificados) {
-		// TODO Auto-generated method stub
-		
+		Collections.sort(jugadoresCalificados);
+		equipo1.add(jugadoresCalificados.get(0).getJugador());
+		equipo2.add(jugadoresCalificados.get(1).getJugador());
+		equipo2.add(jugadoresCalificados.get(2).getJugador());
+		equipo1.add(jugadoresCalificados.get(3).getJugador());
+		equipo1.add(jugadoresCalificados.get(4).getJugador());
+		equipo2.add(jugadoresCalificados.get(5).getJugador());
+		equipo2.add(jugadoresCalificados.get(6).getJugador());
+		equipo1.add(jugadoresCalificados.get(7).getJugador());
+		equipo1.add(jugadoresCalificados.get(8).getJugador());
+		equipo2.add(jugadoresCalificados.get(9).getJugador());
 	}
 
 	
