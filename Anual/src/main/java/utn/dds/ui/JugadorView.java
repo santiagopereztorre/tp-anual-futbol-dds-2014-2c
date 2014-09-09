4f67d2e135558b2f7410b9e1668c9c885512679f
Martin Montenegro <martin.92m@gmail.com>
2014-09-09 13:07:41 -0300

    Creada la vista para el jugador Falta la parte de grilla de amigos y de infracciones

--- Anual/src/main/java/utn/dds/ui/JugadorView.java
+++ Anual/src/main/java/utn/dds/ui/JugadorView.java
@@ -1,5 +1,7 @@
 package utn.dds.ui;
 
+import org.uqbar.arena.layout.VerticalLayout;
+import org.uqbar.arena.widgets.Label;
 import org.uqbar.arena.widgets.Panel;
 import org.uqbar.arena.windows.MainWindow;
 import org.uqbar.arena.windows.Window;
@@ -8,22 +10,51 @@ import org.uqbar.arena.windows.WindowOwner;
 import utn.dds.ui.JugadorViewModel;
 import utn.dds.jugador.Jugador;
 
-
 public class JugadorView extends Window<JugadorViewModel> {
 
 	private static final long serialVersionUID = 1L;
 	private static final WindowOwner GenerarEquiposView = null;
 
-	public JugadorView() {
-	    super(GenerarEquiposView, new JugadorViewModel(new Jugador()));
-	  }
+	public JugadorView(Jugador unJugador)
+	{
+		super(GenerarEquiposView, new JugadorViewModel(unJugador));
+	}
 
 	@Override
-	public void createContents(Panel mainPanel) {
-		// TODO Auto-generated method stub
-		
-	}
-	
+	public void createContents(Panel mainPanel)
+	{
+		setTitle("Visualizar Jugador");
+		mainPanel.setLayout(new VerticalLayout());
 
+		// Nombre
+		new Label(mainPanel).setText("Nombre:");
+		new Label(mainPanel).bindValueToProperty("nombre");
+
+		// Apodo
+		new Label(mainPanel).setText("Apodo:");
+		new Label(mainPanel).bindValueToProperty("apodo");
+
+		// Handicap
+		new Label(mainPanel).setText("Handicap:");
+		new Label(mainPanel).bindValueToProperty("handicap");
+
+		// Promedio del ultimo partido
+		new Label(mainPanel).setText("Promedio ultimo partido:");
+		new Label(mainPanel).bindValueToProperty("promedioUltimoPartido");
+
+		// Promedio de todos los partidos
+		new Label(mainPanel).setText("Promedio todos los partidos:");
+		new Label(mainPanel).bindValueToProperty("promedioGeneral");
+		
+		// Amigos
+		new Label(mainPanel).setText("Amigos:");
+		
+		// Infracciones
+		new Label(mainPanel).setText("Infracciones:");
+		
+		// Cant partidos jugados
+		new Label(mainPanel).setText("Cantidad de partidos:");
+		new Label(mainPanel).bindValueToProperty("cantidadPartidosJugados");
+	}
 
 }
