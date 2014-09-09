404230ad8028dde1c019f15df7b012e47e4c9241
ramabit <f.e.ramirez94@gmail.com>
2014-09-09 14:47:04 -0300

    bindiando atributos en generar equipos

--- Anual/src/main/java/utn/dds/ui/GenerarEquiposView.java
+++ Anual/src/main/java/utn/dds/ui/GenerarEquiposView.java
@@ -1,9 +1,11 @@
 package utn.dds.ui;
 
 import org.uqbar.arena.layout.VerticalLayout;
+import org.uqbar.arena.widgets.Label;
 import org.uqbar.arena.widgets.Panel;
 import org.uqbar.arena.widgets.RadioSelector;
 import org.uqbar.arena.windows.MainWindow;
+
 import utn.dds.partido.Partido;
 
 public class GenerarEquiposView extends MainWindow<GenerarEquiposViewModel> {
@@ -22,8 +24,25 @@ public class GenerarEquiposView extends MainWindow<GenerarEquiposViewModel> {
 	public void createContents(Panel mainPanel) {
 		setTitle("Generar equipos");
 		mainPanel.setLayout(new VerticalLayout());
+		mainPanel.setWidth(400);
+
+		new Label(mainPanel).setText("Criterio de Seleccion de Jugadores");
+		RadioSelector<String> radioSelectorCriterios = new RadioSelector<>(mainPanel);
+		radioSelectorCriterios.setWidth(100);
+		radioSelectorCriterios.bindValueToProperty("criterioSeleccionado");
+		radioSelectorCriterios.bindItemsToProperty("criterios");
 		
-		new RadioSelector<>(mainPanel);
+		new Label(mainPanel).setText("Criterio");
+		new Label(mainPanel).bindValueToProperty("criterioSeleccionado");
+		
+		new Label(mainPanel).setText("Criterio de Ordenamiento de Jugadores");
+		RadioSelector<String> radioSelectorOrdenamiento = new RadioSelector<>(mainPanel);
+		radioSelectorOrdenamiento.setWidth(200);
+		radioSelectorOrdenamiento.bindValueToProperty("ordenamientoSeleccionado");
+		radioSelectorOrdenamiento.bindItemsToProperty("ordenamientos");
+		
+		 new Label(mainPanel).setText("Ordenamiento");
+		 new Label(mainPanel).bindValueToProperty("ordenamientoSeleccionado");
 	}
 	
 	public static void main(String[] args) {
--- Anual/src/main/java/utn/dds/ui/GenerarEquiposViewModel.java
+++ Anual/src/main/java/utn/dds/ui/GenerarEquiposViewModel.java
@@ -1,12 +1,44 @@
 package utn.dds.ui;
 
+import java.util.Arrays;
+import java.util.List;
+
 import utn.dds.equipos.*;
 import utn.dds.partido.Partido;
+import org.uqbar.commons.utils.Observable;
 
+@Observable
 public class GenerarEquiposViewModel {
 
 	public GenerarEquiposViewModel(Partido partido) {
 		// TODO Auto-generated constructor stub
+	}
+	
+	private String criterioSeleccionado;
+	private String ordenamientoSeleccionado;
+	
+	public String getOrdenamientoSeleccionado() {
+		return ordenamientoSeleccionado;
+	}
+
+	public void setOrdenamientoSeleccionado(String ordenamientoSeleccionado) {
+		this.ordenamientoSeleccionado = ordenamientoSeleccionado;
+	}
+
+	public String getCriterioSeleccionado() {
+		return criterioSeleccionado;
+	}
+
+	public void setCriterioSeleccionado(String criterioSeleccionado) {
+		this.criterioSeleccionado = criterioSeleccionado;
+	}	
+	
+	public List<String> getCriterios(){
+		return Arrays.asList("Par/impar", "1,4,5,8,9");
+	}
+	
+	public List<String> getOrdenamientos(){
+		return Arrays.asList("Por hándicap", "Por promedio de notas del último partido", "Por promedio de notas de los últimos n partidos", "Mixto");
 	}
 	
 }
