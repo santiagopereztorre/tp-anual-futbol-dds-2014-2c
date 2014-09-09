4baf04fe6b2940b4165b8371ce4e0b88026a9fbe
Martin Montenegro <martin.92m@gmail.com>
2014-09-09 16:04:48 -0300

    Reordenado codigo para generar tabla infracciones

--- Anual/src/main/java/utn/dds/ui/JugadorView.java
+++ Anual/src/main/java/utn/dds/ui/JugadorView.java
@@ -55,10 +55,7 @@ public class JugadorView extends Window<JugadorViewModel> {
 		
 		// Infracciones
 		new Label(mainPanel).setText("Infracciones:");
-		Table<Infraccion> table = new Table<Infraccion>(mainPanel, Infraccion.class); 
-		table.setHeigth(200); 
-		table.setWidth(300); 
-		armarTablaInfracciones(table);
+		Table<Infraccion> table = armarTablaInfracciones(mainPanel);
 		table.bindItemsToProperty("infracciones");
 		
 		// Cant partidos jugados
@@ -66,8 +63,13 @@ public class JugadorView extends Window<JugadorViewModel> {
 		new Label(mainPanel).bindValueToProperty("cantidadPartidosJugados");
 	}
 	
-	void armarTablaInfracciones(Table<Infraccion> tabla)
+	private Table<Infraccion> armarTablaInfracciones(Panel mainPanel)
 	{
+		Table<Infraccion> tabla = new Table<Infraccion>(mainPanel, Infraccion.class);
+		
+		tabla.setHeigth(200); 
+		tabla.setWidth(300);
+		
 		new Column<Infraccion>(tabla)
 			.setTitle("Fecha")
 			.setFixedSize(100);
@@ -75,6 +77,8 @@ public class JugadorView extends Window<JugadorViewModel> {
 		new Column<Infraccion>(tabla)
 			.setTitle("Motivo")
 			.setFixedSize(100);
+		
+		return tabla;
 	}
 	
 	/*public static void main(String[] args)
