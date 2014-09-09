aa8ebae33802b038197352cae5644d5892852f30
Santiago <saantii.28@hotmail.com>
2014-09-09 15:09:01 -0300

    saco un serial que meti mal

--- Anual/src/main/java/utn/dds/ui/GenerarEquiposView.java
+++ Anual/src/main/java/utn/dds/ui/GenerarEquiposView.java
@@ -4,19 +4,15 @@ import org.uqbar.arena.layout.VerticalLayout;
 import org.uqbar.arena.widgets.Panel;
 import org.uqbar.arena.widgets.RadioSelector;
 import org.uqbar.arena.windows.MainWindow;
+
 import utn.dds.partido.Partido;
 
+@SuppressWarnings("serial")
 public class GenerarEquiposView extends MainWindow<GenerarEquiposViewModel> {
 
 	public GenerarEquiposView() {
 		super(new GenerarEquiposViewModel(new Partido()));
-		// TODO Auto-generated constructor stub
 	}
-
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
 
 	@Override
 	public void createContents(Panel mainPanel) {
