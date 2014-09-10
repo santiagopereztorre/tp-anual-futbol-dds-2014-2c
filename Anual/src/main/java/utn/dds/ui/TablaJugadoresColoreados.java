7836ca7192a6208c34d35e28272408544de6caf8
ramabit <f.e.ramirez94@gmail.com>
2014-09-10 16:36:22 -0300

    comenzando tabla con jugadores coloreados

---
+++ Anual/src/main/java/utn/dds/ui/TablaJugadoresColoreados.java
@@ -0,0 +1,38 @@
+package utn.dds.ui;
+
+import org.uqbar.arena.widgets.Container;
+import org.uqbar.arena.widgets.Panel;
+import org.uqbar.arena.widgets.tables.Column;
+import org.uqbar.arena.widgets.tables.Table;
+
+import utn.dds.infraccion.Infraccion;
+import utn.dds.jugador.Jugador;
+
+public class TablaJugadoresColoreados extends Table<Jugador>{
+
+	public TablaJugadoresColoreados(Container container) {
+		super(container);
+	}
+
+	public Table<Jugador> armarTabla(Panel mainPanel)
+	{
+		Table<Jugador> tabla = new Table<Jugador>(mainPanel, Jugador.class);
+		
+		tabla.setHeigth(200); 
+		tabla.setWidth(300);
+		
+		new Column<Jugador>(tabla)
+			.setTitle("Nombre")
+			.setFixedSize(100)
+			.bindContentsToProperty("nombre");
+		
+		new Column<Jugador>(tabla)
+		.setTitle("Handicap")
+		.setFixedSize(100)
+		.bindContentsToProperty("handicap");
+		
+		return tabla;
+	}
+	private static final long serialVersionUID = 1L;
+
+}
